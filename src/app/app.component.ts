4ee40a1f8c2f12168b125ca1937dc73c1a7ca316
samsagaz <monobedoya@gmail.com>
2018-12-06 21:30:30 -0500

    Complete de point 2 "Displaying a List"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-hero-App';
+  title = 'Tour of Heroes';
 }
